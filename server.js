e3550263645b069d6524b497ff3eb47d0627432d
amribrahim34 <amramr3434@gmail.com>
2021-10-07 20:08:53 +0200

    basic instalation for the required packages

--- server.js
+++ server.js
@@ -2,8 +2,13 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const port = 8000;
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -13,7 +18,9 @@ app.use(bodyParser.json());
 // Cors for cross origin allowance
 
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("website"));
 
 // Setup Server
+app.listen(port, () => {
+  console.log("server is running");
+});
