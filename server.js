db11ce26abf49058bfef5b7c0c30be1ebda21228
amribrahim34 <amramr3434@gmail.com>
2021-10-10 11:43:12 +0200

    the request is fetching data

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let projectData = {};
 
 // Require Express to run server and routes
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+import fetch from "node-fetch";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
 const port = 8000;
-const apiKey = f076f67756a740f2111ede1f6deacf95;
-const zip = document.getElementById('zip').value;
-const apiEndPoint = api.openweathermap.org/data/2.5/weather?zip=zip&appid=apiKey;
+const apiKey = `f076f67756a740f2111ede1f6deacf95`;
+const zip = 85001;
+const apiEndPoint = `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}`;
+const options = {
+  method: "GET",
+};
 
 // Start up an instance of app
 const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
+http: app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -29,13 +34,14 @@ app.listen(port, () => {
   console.log("server is running");
 });
 
-
- let ss = fetch(apiEndPoint);
+let ss = fetch(apiEndPoint);
 console.log(ss);
+// console.log("asda");
 
 // get request that sends the project data
-app.get("/projectdata", (req, res) => {
-  res.send(projectData);
+app.get("/projectdata", async (req, res) => {
+  let projectData = await fetch(apiEndPoint, options).then((res) => res.json());
+  res.json(projectData);
 });
 
 app.post("/projectdata", (req, res) => {
